59b925e9db5161b1b978381a2834bd5349e4bdbd
Pascal Seeland <pascal.seeland@tik.uni-stuttgart.de>
2022-09-07 13:06:08 +0200

    Add uploading a file to a given s3 url

--- src/volumecreator.py
+++ src/volumecreator.py
@@ -7,19 +7,67 @@ from werkzeug.utils import secure_filename
 import glob
 from flask import jsonify
 from flask.helpers import make_response
-
+import requests
+import mimetypes
+import re
+import hashlib
 
 UPLOAD_FOLDER = '/tmp/shared'
-
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SECRET_KEY'] = 'o5sGDoMC6LS2L0XIVBwprbgPADbxb3SJCaGLnzxVbCXr14JiWRGJFvAYq7eJoAA8'
 
 def jsonify_no_content():
     response = make_response('', 204)
     response.mimetype = app.config['JSONIFY_MIMETYPE']
- 
     return response
+
+regex = re.compile(
+                r'^(?:http|ftp)s?://' # http:// or https://
+                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+                r'localhost|' #localhost...
+                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+                r'(?::\d+)?' # optional port
+                r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+def sha256sum(filename):
+    h  = hashlib.sha256()
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        while n := f.readinto(mv):
+            h.update(mv[:n])
+    return h.hexdigest()
+
+def add_mimetypes():
+    mimetypes.init()
+    mimetypes.add_type("application/vnd.kitware", ".vtu")
+    mimetypes.add_type("application/vnd.kitware", ".vtp")
+    mimetypes.add_type("application/x-vgf", ".vgf")
+    mimetypes.add_type("application/x-vgf3", ".vgf3")
+    mimetypes.add_type("application/x-vgfc", ".vgfc")
+
+def get_mimetype(file_path):
+    mime = mimetypes.guess_type(file_path)
+    mimetype = ""
+    if mime[0] == "text/plain":
+        with open(file_path, 'r') as fr:
+            lines = fr.readlines()
+        is_uri = all([re.match(regex, line.strip()) for line in lines])
+        if is_uri:
+            mimetype = "text/uri-list"
+        else:
+            mimetype = "text/plain"
+    elif mime[0] is not None:
+        mimetype = mime[0]
+    else:
+        mimetype = "application/octet-stream"
+    return mimetype
+
+
+# init mimetypes and add missing types
+add_mimetypes()
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'o5sGDoMC6LS2L0XIVBwprbgPADbxb3SJCaGLnzxVbCXr14JiWRGJFvAYq7eJoAA8'
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -50,9 +98,22 @@ def upload_file():
 
 @app.route('/list')
 def list_files():
-    files = [file[len(app.config["UPLOAD_FOLDER"]):] for file in glob.glob("%s/*"%app.config["UPLOAD_FOLDER"] )]
+    files = []
+    for file in glob.glob("%s/*"%app.config["UPLOAD_FOLDER"]) :
+        name = file[len(app.config["UPLOAD_FOLDER"]):]
+        size = os.path.getsize(file)
+        mimetype = get_mimetype(file)
+        files.append({"name": name, "size": size, "mimetype" : mimetype})
     return jsonify(files)
 
 @app.route('/data/<name>')
 def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+@app.route('/upload/<name>', methods=['POST'])
+def post_file_to_s3(name):
+    response = request.json
+    print(response)
+    with open(app.config["UPLOAD_FOLDER"] + '/' + name, 'rb') as f:
+        r = requests.put(response["target"], data=f)
+        return jsonify(r.status_code)
